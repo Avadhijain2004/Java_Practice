552fd5f51e85172b92a9a548eff23bfc5377e7c1
Avadhi <avadhijain2004@gmail.com>
2023-07-29 10:10:42 +0530

    Chapter 1 completed

--- Practice Set 1 Solutions/Sum_of_three_numbers.java
+++ Practice Set 1 Solutions/Sum_of_three_numbers.java
@@ -1,6 +1,6 @@
-/*Java program to calculate sum of three integers  */
+/*Java program to find the sum of 3 Numbers */
 import java.util.Scanner;
-public class Sum_of_3_numbers 
+class Sum_of_three_numbers45
 {
     public static void main(String[] args) 
     {
